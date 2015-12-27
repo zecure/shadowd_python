9e3062dba698f1128b57c7521bd548cb0400cea7
zit-hb <hb@zecure.org>
2015-12-27 13:09:52 +0100

    Added default charset to avoid error for old Django versions.

--- shadowd/tests/__init__.py
+++ shadowd/tests/__init__.py
@@ -1,7 +1,7 @@
 import unittest
 
 def test_all():
-	return unittest.TestLoader().loadTestsFromNames([
+    return unittest.TestLoader().loadTestsFromNames([
         'shadowd.tests.test_connector',
         'shadowd.tests.test_django_connector',
     ])
