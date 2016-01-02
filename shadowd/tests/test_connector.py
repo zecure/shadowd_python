d6f1490c0779b29199d860b8fad91df96c29aa62
zit-hb <hb@zecure.org>
2016-01-02 22:18:39 +0100

    Updated header.

--- shadowd/tests/test_connector.py
+++ shadowd/tests/test_connector.py
@@ -1,3 +1,19 @@
+# Shadow Daemon -- Web Application Firewall
+#
+# Copyright (C) 2014-2016 Hendrik Buchwald <hb@zecure.org>
+#
+# This file is part of Shadow Daemon. Shadow Daemon is free software: you can
+# redistribute it and/or modify it under the terms of the GNU General Public
+# License as published by the Free Software Foundation, version 2.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
 import unittest
 import shadowd.connector
 
